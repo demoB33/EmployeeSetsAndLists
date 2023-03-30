0a1336860b1ee6d80713b6569e9a80d2c02f82c9
demoB33 <vasilenko-s@list.ru>
2023-03-30 14:28:29 +0700

    Initial commit

---
+++ src/main/java/com/example/employeesetsandlists/EmployeeSetsAndListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeesetsandlists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSetsAndListsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSetsAndListsApplication.class, args);
+    }
+
+}
